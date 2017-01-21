81e8c7ea87d225657a3ca2d7e093b297266cad47
Sergey Vlasov <sergeyv@securitydam.com>
2017-01-21 23:25:51 +0200

    - add .vagrant/ and *.log to .gitignore -  add Vagrantfile with ubuntu/xenial64 - add apt-install-basics.sh - improve apt-install-brew.sh - add apt-install-ruby

---
+++ install/apt/apt-install-basics.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+sudo apt-get update
+sudo apt-get install -y \
+aptitude \
+curl \
+wget \
+unzip \
+ack-grep \
+htop \
+vim \
+tmux \
+tree \
+software-properties-common
--- install/apt/apt-install-brew.sh
+++ install/apt/apt-install-brew.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 # source: http://linuxbrew.sh/
+sudo apt-get update
+sudo apt-get install -y build-essential ruby
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 PATH="$HOME/.linuxbrew/bin:$PATH"
 echo 'export PATH="$HOME/.linuxbrew/bin:$PATH"' >>~/.bashrc
-reload
+echo 'export MANPATH="$HOME/.linuxbrew/share/man:$MANPATH"' >>~/.bashrc
+echo 'export INFOPATH="$HOME/.linuxbrew/share/info:$INFOPATH"' >>~/.bashrc
+source ~/.bashrc
+
