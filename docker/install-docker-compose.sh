3bdeffb9479ffca568558dce3d430b0afa95041c
svlasov <im.sergey.vlasov@gmail.com>
2017-03-09 11:24:29 +0200

    Update install-docker-compose.sh

--- docker/install-docker-compose.sh
+++ docker/install-docker-compose.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+VERSION=1.10.0
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
